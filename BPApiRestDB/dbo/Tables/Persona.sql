8ffc59ed483d3fae482c7a6da131ebd421318448
Joel Ontuña Narváez <joelontu@hotmail.com>
2022-09-19 22:34:54 -0500

    Add Person Table

---
+++ BPApiRestDB/dbo/Tables/Persona.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Persona] (
+    [Identificacion] VARCHAR (15)  NOT NULL,
+    [Nombre]         VARCHAR (100) NOT NULL,
+    [Genero]         VARCHAR (15)  NOT NULL,
+    [Edad]           INT           NULL,
+    [Direccion]      VARCHAR (200) NOT NULL,
+    [Telefono]       VARCHAR (15)  NULL,
+    PRIMARY KEY CLUSTERED ([Identificacion] ASC)
+);
+
