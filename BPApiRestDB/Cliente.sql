54290e61b2bbb5663fa1a27a37db1e7514eaaa3f
Joel Ontuña Narváez <joelontu@hotmail.com>
2022-09-20 14:21:40 -0500

    Tabla Cliente

---
+++ BPApiRestDB/Cliente.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Cliente]
+(
+	[ClienteID] INT NOT NULL PRIMARY KEY, 
+    [Identificacion] VARCHAR(15) NOT NULL, 
+    [Contrasenia] VARCHAR(20) NOT NULL, 
+    [Estado] VARCHAR(20) NOT NULL, 
+    CONSTRAINT [FK_ClientePersona] FOREIGN KEY ([Identificacion]) REFERENCES [dbo].[Persona]([Identificacion])
+    )
